f0bb29f01ae61fce0928757552c278506d885b63
Marcos Antonio <marcosantoniotaina@gmail.com>
2019-10-09 10:32:49 -0300

    create model products

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'home#index'
 
   get '/budget' => 'home#budget'
   get '/news' => 'home#news'
+
+  resources :products, only:[:show]
 end
