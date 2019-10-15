dae2c33d443013932a951efd71e3a816e7665de7
Marcos Antonio <marcosantoniotaina@gmail.com>
2019-10-15 01:32:44 -0300

    create admin panel and more options for products

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
   get '/budget' => 'home#budget'
   get '/news' => 'home#news'
 
-  resources :products, only:[:show]
+  get 'admin/products/new' => 'admin#new_product'
+  get 'admin/products' => 'admin#products'
+  get 'admin/products/:id' => 'admin#show_product', as: :admin_product
+  get 'admin/products/:id/edit' => 'admin#edit_product', as: :admin_edit_product
+
+  resources :products, only:[:create, :update, :index, :show]
+  resources :admin, only:[:index]
 end
